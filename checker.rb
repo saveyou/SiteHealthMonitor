ed59331dd4efed74b73f4e43224baf32c1c466a3
saveyou <rangga.sidiq@gmail.com>
2016-12-14 14:40:05 +0700

    Support multiple site

--- checker.rb
+++ checker.rb
@@ -14,22 +14,31 @@ def send_alert(message)
 end
 
 #HTTP.timeout(:global, :write => 1, :connect => 1, :read => 1)
-begin
-  resp = HTTP.get(SITE)
-  if resp.status.to_i == 200
-    p "Live"
-    now = Time.now
-    hour = now.strftime('%H')
-    minute =  now.strftime('%M')
-    send_alert(SAFE_MESSAGE) if ["08","15","20"].include?(hour) && minute == "00"
-  else
-    p "Site Down"
-    send_alert(WARNING_MESSAGE)
-  end
-rescue Exception => e
-  puts e.message
-  puts e.backtrace.inspect
-  p "Site Down"
-  send_alert(WARNING_MESSAGE)
+def process(site)
+	begin
+	  resp = HTTP.get(site)
+	  p site
+	  if resp.status.to_i == 200
+	    p "Live"
+	    now = Time.now
+	    hour = now.strftime('%H')
+	    minute =  now.strftime('%M')
+	    send_alert(format(SAFE_MESSAGE, site)) if ["08","15","20"].include?(hour) && minute == "00"
+	  else
+	    p "Site Down"
+	    send_alert(format(WARNING_MESSAGE, site))
+	  end
+	rescue Exception => e
+	  puts e.message
+	  puts e.backtrace.inspect
+	  p "Site Down"
+	  send_alert(format(WARNING_MESSAGE, site))
+	end
 end
+
+SITES.each do |s|
+	process(s)
+	p "======="
+end
+
 p "Done"
